201d1e1ada2c3a8dbdf4743f3b3060b0b14b1a6a
836948082 <836948082@qq.com>
2016-07-22 11:26:29 +0800

    CatLoadingView

---
+++ README.md
@@ -0,0 +1,44 @@
+# Android CatLoadingView
+
+This project idea is from [Link](http://mp.weixin.qq.com/s?__biz=MjM5MDMxOTE5NA==&mid=402703079&idx=2&sn=2fcc6746a866dcc003c68ead9b68e595&scene=2&srcid=0302A7p723KK8E5gSzLKb2ZL&from=timeline&isappinstalled=0#wechat_redirect) .<br>
+Thanks for the idea.<br>
+
+I like the animation in that picture.<br>
+
+![](http://ww4.sinaimg.cn/mw690/a695acdejw1f2hbmfxzk5g208u0a2ass.gif)
+
+..as you see right now,wish you like it.
+
+### Step
+
+Import this project into android studio..it's build with it.
+
+###  Usage
+
+#### Gradle
+
+```
+compile 'com.android.support:design:23.2.1'
+```
+
+####  config in java code
+
+    CatLoadingView mView;
+
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        mView = new CatLoadingView();
+        findViewById(R.id.button).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override public void onClick(View v) {
+                        mView.show(getSupportFragmentManager(), "");
+                    }
+                });
+    }
+
+## TODO
+
+This view is adjust in Nexue5 but not test in the other size screen .
---
+++ app/src/main/java/com/runtai/catloadingview/MainActivity.java
@@ -0,0 +1,44 @@
+package com.runtai.catloadingview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.runtai.catloadingview.library.CatLoadingView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog();
+            }
+        });
+    }
+
+    public void showDialog() {
+        new CatLoadingView().show(getSupportFragmentManager(), "");
+    }
+
+    @Override public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
